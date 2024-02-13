e00798a3bce3ce80b297906153935ea4d968c097
Samarth1002 <samarthanandkamble1002@gmail.com>
2024-02-14 00:23:27 +0530

    configured store for form fields

---
+++ src/utils/Redux/formSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const formSlice = createSlice({
+  name: "form",
+  initialState: {
+    formFields: [],
+  },
+  reducers: {
+    addField: (state, action) => {
+      state.formFields.push(action.payload);
+    },
+    removeField: (state, action) => {
+      return state.formFields.filter(
+        (field, index) => index !== action.payload
+      );
+    },
+    updateField: (state, action) => {
+      const { index, value } = action.payload;
+      state.formFields[index].value = value;
+    },
+  },
+});
+
+export const { addField, removeField, updateField } = formSlice.actions;
+
+export default formSlice.reducer;
